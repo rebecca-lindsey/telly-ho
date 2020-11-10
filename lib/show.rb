e83fde640392a96f6a63b5d24f3944725ff737c1
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-10 14:53:02 -0500

    Fixes Show.list_show_by_type to work dynamically

--- lib/show.rb
+++ lib/show.rb
@@ -34,22 +34,11 @@ class Show
     @@uniq_show_names.sort.each_with_index { |show, index| puts "#{index + 1}. #{show}" }
   end
 
-  def self.list_shows_by_type(type)
-    type_arr = []
-    puts type.to_s
-    @@all.each do |show|
-      if show.type == type
-        type_arr << show
-        puts "#{type_arr.length}. #{show.name}"
-      end
-    end
-    if type_arr.empty?
-      puts "There are no #{type} programs on the selected date. Please choose another type:"
-      Cli.choose_type
-    end
-  end
-
   # rubocop:disable Layout/LineLength
+
+  def self.list_shows_by_type(type)
+    @@all.filter { |show| show.type == type }.sort_by(&:name).each_with_index { |show, index| puts "#{index + 1}. #{show.name}" }
+  end
 
   def self.list_shows_by_genre(genre)
     @@all.filter { |show| show.genre.any?(genre) unless show.genre.nil? }.sort_by(&:name).each_with_index { |show, index| puts "#{index + 1}. #{show.name}" }
