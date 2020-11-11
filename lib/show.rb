72924e70addf95476688b55fb0a20b3ad410cb56
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-11 15:27:23 -0500

    Cleaned up code

--- lib/show.rb
+++ lib/show.rb
@@ -29,40 +29,6 @@ class Show
   def self.types
     @@types
   end
-
-  def self.list_all_shows
-    @@all.sort_by(&:name).uniq(&:name).each_with_index { |show, index| puts "#{index + 1}. #{show.name}" }
-  end
-
-  # rubocop:disable Layout/LineLength
-
-  def self.list_shows_by_type(type)
-    show_list = @@all.filter { |show| show.type == type }.sort_by(&:name).uniq(&:name).each_with_index { |show, index| puts "#{index + 1}. #{show.name}" }
-    # Cli.select_show_validation(show_list.length)
-  end
-
-  def self.list_shows_by_genre(genre)
-    genre_list = @@all.filter { |show| show.genre.any?(genre) unless show.genre.nil? }.sort_by(&:name).uniq(&:name).each_with_index { |show, index| puts "#{index + 1}. #{show.name}" }
-    Cli.genre_select_show_validation(genre_list)
-  end
-  # rubocop:enable Layout/LineLength
-
-  def self.access_show_info(name)
-    @@all.filter { |show| show.name == name }
-  end
-
-  def self.display_show(show)
-    puts show.name.to_s.colorize(:blue).bold
-    puts "Status: #{show.status}"
-    puts "Premier date: #{show.premier_date}"
-    puts "Genre: #{show.genre.join(', ')}"
-    puts "Air time: #{show.schedule[0]} on #{show.schedule[1].join(', ')}"
-    puts "Network: #{show.network}"
-    puts 'Summary: '.colorize(:magenta)
-    puts show.summary.to_s.gsub(%r{<\w*>|</\w>}, '').to_s
-    puts ''
-    Cli.return_options
-  end
 end
 # airtime - runtime - summary - show
 # With show: name - status - premiered - schedule - rating - network - webChannel -summary
