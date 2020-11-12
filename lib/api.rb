be6d7a62b03c3d8f8afe0cd970030c2d8c697370
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-12 11:14:36 -0500

    Adds fail-safes for varied API input, corrects messaging

--- lib/api.rb
+++ lib/api.rb
@@ -20,12 +20,11 @@ class Api
   def create_shows
     @show_list.each do |show_hash|
       show_instance = Show.new(name: show_hash['show']['name'], genre: show_hash['show']['genres'], type: show_hash['show']['type'])
-      show_instance.summary = show_hash['show']['summary']
-      show_instance.premier_date = show_hash['show']['premiered']
-      show_instance.status = show_hash['show']['status']
-      show_instance.schedule = [show_hash['show']['schedule']['time'], show_hash['show']['schedule']['days']]
-      show_instance.network = show_hash['show']['network']['name']
-      show_instance.web_channel = show_hash['show']['webChannel']
+      show_hash['show']['summary'].nil? ? show_instance.summary = 'Unlisted' : show_instance.summary = show_hash['show']['summary']
+      show_hash['show']['premier_date'].nil? ? show_instance.premier_date = 'Unlisted' : show_instance.premier_date = show_hash['show']['premiered']
+      show_instance.status = (show_hash['show']['status'].nil? ? 'Unlisted' : show_hash['show']['status'])
+      show_hash['show']['schedule'].nil? ? show_instance.schedule = 'Unlisted' : show_instance.schedule = [show_hash['show']['schedule']['time'], show_hash['show']['schedule']['days']]
+      show_hash['show']['network'].nil? ? show_instance.network = 'Unlisted' : show_instance.network = show_hash['show']['network']['name']
     end
   end
 end
