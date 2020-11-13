0dd8d709c4bdf5addc6cafb5cb4aa0ba7cf715ac
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-13 10:57:19 -0500

    Cleans up Api#create_shows and fixes Gemfile & Environment

--- lib/api.rb
+++ lib/api.rb
@@ -17,12 +17,13 @@ class Api
 
   def create_shows
     @show_list.each do |show_hash|
-      show_instance = Show.new(name: show_hash['show']['name'], genre: show_hash['show']['genres'], type: show_hash['show']['type'])
-      show_hash['show']['summary'].nil? ? show_instance.summary = 'Unlisted' : show_instance.summary = show_hash['show']['summary']
-      show_hash['show']['premiered'].nil? ? show_instance.premier_date = 'Unlisted' : show_instance.premier_date = show_hash['show']['premiered']
-      show_hash['show']['status'].nil? ? show_instance.status = 'Unlisted' : show_instance.status = show_hash['show']['status']
-      show_hash['show']['schedule'].nil? ? show_instance.schedule = 'Unlisted' : show_instance.schedule = [show_hash['show']['schedule']['time'], show_hash['show']['schedule']['days']]
-      show_hash['show']['network'].nil? ? show_instance.network = 'Unlisted' : show_instance.network = show_hash['show']['network']['name']
+      show_info = show_hash['show']
+      show_instance = Show.new(name: show_info['name'], genre: show_info['genres'], type: show_info['type'])
+      show_info['summary'].nil? ? show_instance.summary = 'Unlisted' : show_instance.summary = show_info['summary']
+      show_info['premiered'].nil? ? show_instance.premier_date = 'Unlisted' : show_instance.premier_date = show_info['premiered']
+      show_info['status'].nil? ? show_instance.status = 'Unlisted' : show_instance.status = show_info['status']
+      show_info['schedule'].nil? ? show_instance.schedule = 'Unlisted' : show_instance.schedule = [show_info['schedule']['time'], show_info['schedule']['days']]
+      show_info['network'].nil? ? show_instance.network = 'Unlisted' : show_instance.network = show_info['network']['name']
     end
   end
 end
